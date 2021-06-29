c9558c108046f046cf7f44bb9340d7b292356fe3
steven <steven@mylinux>
2021-06-29 17:49:50 +0800

    initial

--- bin/start-dev1-healthcheck.sh
+++ bin/start-dev1-healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-HEALTH_HOME=/home/oracle/gitrepository/transglobe/streamingetl-common
+HEALTH_HOME=/home/steven/gitrepo/transglobe/streamingetl-common
 
 java -cp "${HEALTH_HOME}/target/streamingetl-common-1.0.jar:${HEALTH_HOME}/lib/*" -Dprofile.active=env-dev1 com.transglobe.streamingetl.common.app.HealthCheckApp
